055708a40978d58d22748c92344db9a310e4069c
Aravinthan.N <aravinth700000@gmail.com>
2024-06-16 16:03:31 +0530

    new changes

--- src/Components/Controls/Dockmenu.js
+++ src/Components/Controls/Dockmenu.js
@@ -1,12 +1,45 @@
+import React, { useRef, useState, forwardRef, useImperativeHandle, useEffect } from 'react';
 import { Dock } from 'primereact/dock';
-import React,{useEffect} from 'react';
-const Dockmenu=()=>{
+import PropTypes from 'prop-types';
 
+const Dockmenu = forwardRef((props, ref) => {
+  // Destructuring props
+  const { custom } = props;
 
-    return(
-        <React.Fragment>
-            
-        </React.Fragment>
-    )
-}
-export default Dockmenu;
+  // State variables
+  const [position, setPosition] = useState(custom.position);
+  const [items, setItems] = useState(custom.items);
+
+  // Ref for the Dock component
+  const refCurrentElement = useRef();
+
+  // Update state when props change
+  useEffect(() => {
+    setPosition(custom.position);
+    setItems(custom.items);
+  }, [custom]);
+
+  // Expose method to parent component using ref
+  useImperativeHandle(ref, () =>refCurrentElement.current );
+
+  return (
+    <div  className="dock-window">
+         <Dock ref={refCurrentElement} model={items} position={position} />
+    </div>
+   
+  );
+});
+
+// Define PropTypes for Dockmenu component
+Dockmenu.propTypes = {
+  custom: PropTypes.shape({
+    position: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      icon: PropTypes.string.isRequired
+    })).isRequired,
+    // Add more PropTypes as needed
+  }).isRequired,
+};
+
+export default React.memo(Dockmenu);
--- src/Pages/Adminpage.js
+++ src/Pages/Adminpage.js
@@ -1,40 +1,56 @@
-import React,{useState,useEffect} from 'react';
+import React from 'react';
 import adminbg from'../Image/Adminstrator-home/admin-bg-v7.gif';
 import '../CSS/adminhome.css'
+import Dockmenu from '../Components/Controls/Dockmenu';
+import logout from '../Image/menu-images/power-btn.png'
+import back from '../Image/menu-images/left-arrow-back.png'
 export default function Adminpage() {
-  let lapagedetails=[
-    {'page':"Query Page","id":1,"icon":"bi bi-person-raised-hand"},{"page":"Asset Page","id":2,"icon":"bi bi-pc-display-horizontal"},{"page":"Role Page","id":3,"icon":'bi bi-person-rolodex'}
-  ]
+ 
   const lostyle={
     backgroundImage: `url(${adminbg})`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     height:'100vh'
-    
   }
+  let lapagedetails=[
+    {'page':"Query Page","id":1,"icon":"bi bi-person-raised-hand"},{"page":"Asset Page","id":2,"icon":"bi bi-pc-display-horizontal"},{"page":"Role Page","id":3,"icon":'bi bi-person-rolodex'}
+  ]
   let lahover=['border-green-500','border-pink-500','border-yellow-500']
-  let lashovertxt=["green",'pink','yellow']  
+  let lashovertxt=["green",'pink','yellow'] ;
+  let ladockmenuitems=[
+    {label: 'Logout',
+    icon: () => <img title='logout' alt="logout" src={logout} width="100%" />,
+    command: () => {
+      alert('hi')
+    }},{label: 'bacl',
+      icon: () => <img title='logout' alt="logout" src={back} width="100%" />,
+      command: () => {
+        alert('hi')
+      }}
+
+  ]
+  
   return (
     <React.Fragment>
       <div style={lostyle}>
         <div className='centered-container'>
         <div className="grid grid-nogutter">
-     {lapagedetails.map((product,index)=>{
-        return(
-          <div className={'hover:'+lahover[index] +' border-round  border-3 m-2 flex align-items-center justify-content-center'}style={{backdropFilter:`blur(${'14px'})`,width:'200px',textAlign:'center',color:'white'}  } >
-          <div className="col" key={product.id}>
-          <i className={product.icon + ' p-2'} style={{ display: 'inline-block' }}></i>
- <p className='p-2' style={{ display: 'inline-block', verticalAlign: 'top' }}>{product.page}</p>
+                {lapagedetails.map((product,index)=>{
+                    return(
+                         <div className={'hover:'+lahover[index] +' border-round  border-3 m-2 flex align-items-center justify-content-center'}style={{backdropFilter:`blur(${'14px'})`,width:'200px',textAlign:'center',color:'white'}  } >
+                           <div className="col" key={product.id}>
+                               <i className={product.icon + ' p-2'} style={{ display: 'inline-block' }}></i>
+                                <p className='p-2' style={{ display: 'inline-block', verticalAlign: 'top' }}>{product.page}</p>
 
-          </div>
-       </div>
-        )
-      })} 
-    </div>
-        </div>
+                                </div>
+                            </div>
+                           )
+                         })} 
+                        </div>
+                      </div>
       
-      </div>
-      
+                      </div>
+      <Dockmenu custom={{position:'left',items:ladockmenuitems}} />
     </React.Fragment>
   )
 }
