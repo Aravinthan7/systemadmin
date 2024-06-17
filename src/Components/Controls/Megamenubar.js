6679f7ad0fa2ae47696554ddd7d9d5a2c60b0155
Aravinthan.N <aravinth700000@gmail.com>
2024-06-17 20:34:59 +0530

    megamenubar changes

---
+++ src/Components/Controls/Megamenubar.js
@@ -0,0 +1,21 @@
+import React, { useRef, useState, forwardRef, useImperativeHandle, useEffect } from 'react';
+import { MegaMenu } from 'primereact/megamenu';
+import PropTypes from 'prop-types';
+
+const Megamenubar=forwardRef((props,ref)=>{
+    let {custom:locustom}=props
+    return(
+        <React.Fragment>
+             <div className="card">
+            <MegaMenu model={items} orientation="horizontal" start={start} end={end} breakpoint="960px" className="p-3 surface-0 shadow-2" style={{ borderRadius: '3rem' }} />
+        </div>
+        </React.Fragment>
+    )
+})
+Megamenubar.propTypes = {
+    items: PropTypes.array.isRequired,
+    start: PropTypes.string.isRequired,
+    end: PropTypes.string.isRequired,
+};
+
+export default React.memo(Megamenubar);
