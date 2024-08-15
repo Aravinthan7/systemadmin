7126f086f4c7b1acede14764c8666b903e4a7b93
Aravinthan.N <aravinth700000@gmail.com>
2024-08-15 19:57:27 +0530

    redux toolkit was handled

--- src/app/slices/userSlices.js
+++ src/app/slices/userSlices.js
@@ -1,12 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState={
+    users:[]
+}
+
 export const userSlice = createSlice({
     name:'user',
-    initialState:[],
+    initialState,
     reducers:{
-        addUser:(state,action)=>{
-            
+        setUsers:(state,action)=>{
+            state.users=[...state.users,action.payload];
+        },
+        deleteUsers:(state,action)=>{
+            state.users=[]
         }
     }
 
 })
+export const {setUsers,deleteUsers}=userSlice.actions;
+
+export default userSlice.reducer;
--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
+import userReduser from './slices/userSlices';
 
-export default configureStore({
+ const store = configureStore({
     reducer:{
-
+        userInfo:userReduser,
     }
 })
+
+export default store;
